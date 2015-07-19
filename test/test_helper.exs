39e8f5d7de692577fbf003fca352a956f0dc4209
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-07-18 17:23:08 -0700

    Moved up the chain

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,11 +1,12 @@
 ExUnit.start
+HTTPoison.start
 
 # Create the database, run migrations, and start the test transaction.
 Mix.Task.run "ecto.create", ["--quiet"]
 Mix.Task.run "ecto.migrate", ["--quiet"]
 Ecto.Adapters.SQL.begin_test_transaction(LinkExtractorWeb.Repo)
 
-HTTPoison.start
+
 :timer.sleep 2000
 
 
