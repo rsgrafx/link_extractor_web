54b4a5b40feba6b6a527c8f5541d20c3ef699e5b
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-07-20 00:24:31 -0700

    removed manifest file from prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :link_extractor_web, LinkExtractorWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "example.com", port: 80]
+  # cache_static_manifest: "priv/static/manifest.json"
 
 # ## SSL Support
 #
